2058369567ff07721f17eb3c57b463069a69b4ad
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2024-01-08 05:25:05 -0300

    feat(auth): protected endpoints

--- lib/real_deal_api_web/router.ex
+++ lib/real_deal_api_web/router.ex
@@ -2,19 +2,20 @@ defmodule RealDealApiWeb.Router do
   use RealDealApiWeb, :router
   use Plug.ErrorHandler
 
-  defp handle_errors(conn, %{reason: %Phoenix.Router.NoRouteError{message: message}}) do
-    conn
-    |> json(%{error: message})
-    # stop any downstream plugs from being executed after the errors called.
-    |> halt()
+  def handle_errors(conn, %{reason: %Phoenix.Router.NoRouteError{message: message}}) do
+    conn |> json(%{errors: message}) |> halt()
   end
 
-  defp handle_errors(conn, %{reason: %{message: message}}) do
-    conn |> json(%{error: message}) |> halt()
+  def handle_errors(conn, %{reason: %{message: message}}) do
+    conn |> json(%{errors: message}) |> halt()
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :auth do
+    plug RealDealApiWeb.Auth.Pipeline
   end
 
   scope "/api", RealDealApiWeb do
@@ -22,5 +23,10 @@ defmodule RealDealApiWeb.Router do
     get "/", DefaultController, :index
     post "/accounts/create", AccountController, :create
     post "/accounts/sign_in", AccountController, :sign_in
+  end
+
+  scope "/api", RealDealApiWeb do
+    pipe_through [:api, :auth]
+    get "/accounts/by_id/:id", AccountController, :show
   end
 end
