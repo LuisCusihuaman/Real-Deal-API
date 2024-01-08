fd801d38eff44b6377d856c6513edcf5d5976e50
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2024-01-08 08:12:44 -0300

    feat: refresh_session endpoint to AccountController

--- lib/real_deal_api_web/controllers/account_controller.ex
+++ lib/real_deal_api_web/controllers/account_controller.ex
@@ -49,6 +49,29 @@ defmodule RealDealApiWeb.AccountController do
     end
   end
 
+  def refresh_session(conn, %{}) do
+    old_token = Guardian.Plug.current_token(conn)
+
+    case Guardian.decode_and_verify(old_token) do
+      {:ok, claims} ->
+        case Guardian.resource_from_claims(claims) do
+          {:ok, account} ->
+            {:ok, _old, {new_token, _new_claims}} = Guardian.refresh(old_token)
+
+            conn
+            |> Plug.Conn.put_session(:account_id, account.id)
+            |> put_status(:ok)
+            |> render("account_token.json", %{account: account, token: new_token})
+
+          {:error, _reason} ->
+            raise ErrorResponse.NotFound
+        end
+
+      {:error, _reason} ->
+        raise ErrorResponse.NotFound
+    end
+  end
+
   @spec sign_out(Plug.Conn.t(), any()) :: Plug.Conn.t()
   def sign_out(conn, %{}) do
     account = conn.assigns[:account]
